b59bc37f1c30c1e3d49384fe9f7b3e0e6fcfc9fe
David Gugea <davidgugea@yahoo.com>
2023-03-31 23:02:27 +0200

    docs

---
+++ ReactRouter/reactrouter-test/src/main.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom';
+import './index.css'
+
+import Root from './routes/root';
+import Contact from './routes/contact';
+import ErrorPage from './routes/error-page';
+
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Root />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: 'contacts/:contactId',
+      element: <Contact />,
+    }
+  ]
+)
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>
+)
