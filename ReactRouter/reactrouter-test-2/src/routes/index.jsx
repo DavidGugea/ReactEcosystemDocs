aaf2054d7f8926a4e0fe10331f07aa0c28e26937
David Gugea <davidgugea@yahoo.com>
2023-04-01 15:21:19 +0200

    React Router Docs

---
+++ ReactRouter/reactrouter-test-2/src/routes/index.jsx
@@ -0,0 +1,10 @@
+export default function Index() {
+    return (
+        <p id="zero-state">
+            This is a demo for react Router
+            <br />
+            Check out{" "}
+            <a href="https://www.reactrouter.com">the docs at reactrouter.com</a>
+        </p>
+    )
+}
